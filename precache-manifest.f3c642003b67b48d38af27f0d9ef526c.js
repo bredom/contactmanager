5c1dc4e7e1f6e307a349894dbc23af8ee0791ef9
bredom <domm747@gmail.com>
2019-04-24 23:50:36 +0200

    Updates

---
+++ precache-manifest.f3c642003b67b48d38af27f0d9ef526c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a9b80a0b0e722e0d3664",
+    "url": "/contactmanager/static/js/runtime~main.884ecd02.js"
+  },
+  {
+    "revision": "7c5136d2c021b806194c",
+    "url": "/contactmanager/static/js/main.5c8e9258.chunk.js"
+  },
+  {
+    "revision": "aec7e88081d31934141b",
+    "url": "/contactmanager/static/js/2.0d896f47.chunk.js"
+  },
+  {
+    "revision": "7c5136d2c021b806194c",
+    "url": "/contactmanager/static/css/main.e6257bfd.chunk.css"
+  },
+  {
+    "revision": "aec7e88081d31934141b",
+    "url": "/contactmanager/static/css/2.c17620fa.chunk.css"
+  },
+  {
+    "revision": "8f106e782b07ba5a5850cac59dffde23",
+    "url": "/contactmanager/index.html"
+  }
+];
